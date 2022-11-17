0a2e8ab628994c631663b2812970aeb9df2baca4
Adela-jelimalai <adela.jelimalai@gmail.com>
2022-11-17 18:05:59 +0200

    Push Site

---
+++ js/new.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NX2FDCJTPE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NX2FDCJTPE');
+</script>// JavaScript Document
